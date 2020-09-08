96fba2ce03905c01abeae9d99ede8a92154dcc18
Vicky Parmar <65457337+vkyprmr@users.noreply.github.com>
2020-09-08 23:41:40 +0200

    moving on to rnn

---
+++ Courses/deeplearning.AI/TensorFLow/nlp_inTF/sarcasm_embeddings.py
@@ -0,0 +1,147 @@
+'''
+Developer: vkyprmr
+Filename: sarcasm_embeddings.py
+Created on: 2020-09-08 at 22:29:51
+'''
+'''
+Modified by: vkyprmr
+Last modified on: 2020-09-08 at 23:14:19
+'''
+
+#%%
+# Imports
+import json
+import numpy as np
+import pandas as pd
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, Flatten, GlobalAveragePooling1D
+from tensorflow.keras.optimizers import RMSprop, Adam
+
+import matplotlib.pyplot as plt
+%matplotlib qt
+
+#%%
+# Data
+# Tokenizing sarcasm data
+with open("Data/Sarcasm/coursera/sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+urls = []
+
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+    urls.append(item['article_link'])
+
+#%%
+# Setting hyperparameters for the embeddings
+vocab_size = 10000
+embedding_dim = 16
+max_length = 100
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+training_size = int(0.8*len(sentences))
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+#%%
+# Tokenizing
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
+
+word_index = tokenizer.word_index
+
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+#%%
+# Converting to array
+training_sentences = np.array(training_padded)
+training_labels = np.array(training_labels)
+testing_sentences = np.array(testing_padded)
+testing_labels = np.array(testing_labels)
+
+#%%
+# Building the MODEL
+model = Sequential(
+                    [
+                        Embedding(vocab_size, embedding_dim, input_length=max_length),
+                        GlobalAveragePooling1D(),
+                        Dense(24, activation='relu'),
+                        Dense(1, activation='sigmoid')
+                    ]
+                    )
+model.compile(loss='binary_crossentropy',optimizer=Adam(lr=0.01),metrics=['accuracy'])
+model.summary()
+
+#%%
+# Training
+epochs = 30
+history = model.fit(training_padded, training_labels, epochs=epochs, validation_data=(testing_padded, testing_labels), verbose=1)
+
+#%%
+# Visualizing Accuracy and Loss during training
+def plot_graphs(history, metric):
+  plt.plot(history.history[metric])
+  plt.plot(history.history['val_'+metric])
+  plt.xlabel("Epochs")
+  plt.ylabel(metric)
+  plt.show()
+  
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
+
+#%%
+# Remapping words
+reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
+
+def decode_sentence(text):
+    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+
+print(decode_sentence(training_padded[0]))
+print(training_sentences[0])
+print(labels[0])
+
+#%%
+# Visualization and weights
+e = model.layers[0]
+weights = e.get_weights()[0]
+print(weights.shape) # shape: (vocab_size, embedding_dim)
+
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(1, vocab_size):
+  word = reverse_word_index[word_num]
+  embeddings = weights[word_num]
+  out_m.write(word + "\n")
+  out_v.write('\t'.join([str(x) for x in embeddings]) + "\n")
+out_v.close()
+out_m.close()
+
+#%%
+# Prediction
+sentence = ["granny starting to fear spiders in the garden might be real", "game of thrones season finale showing this sunday night"]
+sequences = tokenizer.texts_to_sequences(sentence)
+padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+print(model.predict(padded))
+
+#%%
+# Colab Part
+try:
+  from google.colab import files
+except ImportError:
+  pass
+else:
+  files.download('vecs.tsv')
+  files.download('meta.tsv')
