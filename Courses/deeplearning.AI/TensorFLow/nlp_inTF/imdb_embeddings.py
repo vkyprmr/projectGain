a837328e2a6cb3c339dade181d6c36b2658e67cf
vkyprmr <parmarvickyk@gmail.com>
2020-09-08 22:10:52 +0200

    gpu config enabled

--- Courses/deeplearning.AI/TensorFLow/nlp_inTF/imdb_embeddings.py
+++ Courses/deeplearning.AI/TensorFLow/nlp_inTF/imdb_embeddings.py
@@ -21,14 +21,14 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 # Set parameters
 """ 
     tf.enable_eager_excecution      # if using tensorflow 1.x
-    from tensorflow.compat.v1 import ConfigProto
-    from tensorflow.compat.v1 import InteractiveSession
-    physical_devices = tf.config.experimental.list_physical_devices('GPU') 
-    for physical_device in physical_devices: 
-        tf.config.experimental.set_memory_growth(physical_device, True)
+    config = tf.compat.v1.ConfigProto(device_count = {'GPU': 1})
+    sess = tf.compat.v1.Session(config=config)
  """
-config = tf.compat.v1.ConfigProto(device_count = {'GPU': 1})
-sess = tf.compat.v1.Session(config=config)
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+physical_devices = tf.config.experimental.list_physical_devices('GPU') 
+for physical_device in physical_devices: 
+    tf.config.experimental.set_memory_growth(physical_device, True)
 
 #%%
 # Loading data from TFDS
