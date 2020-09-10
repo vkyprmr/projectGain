108fdd766bdc5577f26bec6dfb214030d2f5bcd3
Vicky Parmar <65457337+vkyprmr@users.noreply.github.com>
2020-09-10 20:01:50 +0200

    started Sequence, TimeSeries and Prediction

---
+++ Courses/deeplearning.AI/TensorFLow/seq_ts/singlayer.py
@@ -0,0 +1,132 @@
+'''
+Developer: vkyprmr
+Filename: singlayer.py
+Created on: 2020-09-10 at 16:07:04
+'''
+'''
+Modified by: vkyprmr
+Last modified on: 2020-09-10 at 16:32:08
+'''
+
+#%%
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib qt
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.metrics import mean_absolute_error
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+physical_devices = tf.config.experimental.list_physical_devices('GPU') 
+for physical_device in physical_devices: 
+    tf.config.experimental.set_memory_growth(physical_device, True)
+
+#%%
+# Data
+def plot_series(time, series, format="-", start=0, end=None, label=None):
+    plt.plot(time[start:end], series[start:end], format, label=label)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    if label:
+        plt.legend(fontsize=14)
+    plt.grid(True)
+
+### Adding trend to a series
+def trend(time, slope=0):
+    return slope * time
+
+### Adding seasonality
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.3,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.sin(3 * season_time))
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+"""
+    Time series with seasonality and trend
+"""
+time = np.arange(4 * 365 + 1)
+baseline = 10
+slope = 0.1
+amplitude = 40
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+
+### Noise (sounds more practical)
+def white_noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+noise_level = 5
+noise = white_noise(time, noise_level, seed=42)
+
+"""
+    Adding noise to the series
+ """
+series += noise
+
+plot_series(time, series)
+
+### Try to predict it
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+#%%
+# Fixed parameters
+window_size = 15
+batch_size = 32
+shuffle_buffer_size = 1000
+
+#%%
+# Preparing data (features and labels with TF)
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+#%%
+# Building the model
+l0 = tf.keras.layers.Dense(1, input_shape=[window_size])
+model = tf.keras.models.Sequential([l0])
+model.compile(loss="mse", optimizer=SGD(lr=1e-6, momentum=0.9))
+
+#%%
+# Training
+model.fit(dataset,epochs=100,verbose=1)
+print(f'Layer weights: {l0.get_weights()}')
+
+#%%
+# Predictions
+forecast = []
+# fig, ax = plt.subplots()
+for time in range(len(series) - window_size):
+    pred = model.predict(series[time:time + window_size][np.newaxis])
+    print(f'Prediction for {time} time-step: {pred}. \nActual: {series[time]}')
+    forecast.append(pred)
+    # ax.plot(time, pred)
+
+forecast = forecast[split_time-window_size:]
+results = np.array(forecast)[:, 0, 0]
+plot_series(time_train, x_train)
+plot_series(time_valid, x_valid)
+plot_series(time_valid, results)
+
+print(f'MAE: {mean_absolute_error(x_valid, results).numpy()}')
+
+# %%
---
+++ Courses/deeplearning.AI/TensorFLow/seq_ts/syntheticpred.py
@@ -0,0 +1,132 @@
+'''
+Developer: vkyprmr
+Filename: syntheticpred.py
+Created on: 2020-09-10 at 14:45:26
+'''
+'''
+Modified by: vkyprmr
+Last modified on: 2020-09-10 at 15:35:27
+'''
+
+#%%
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib qt
+from pandas.plotting import autocorrelation_plot
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+#%%
+# Data (synthetic)
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+
+def trend(time, slope=0):
+    return slope * time
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.3,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.sin(3 * season_time))
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+def noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+time = np.arange(4 * 365 + 1, dtype="float32")
+baseline = 10
+series = trend(time, 0.1)  
+baseline = 10
+amplitude = 40
+slope = 0.05
+noise_level = 5
+
+# Create the series
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+# Update with noise
+series += noise(time, noise_level, seed=42)
+
+plot_series(time, series)
+
+#%%
+# Splitting data and visualizing
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+plot_series(time_train, x_train)
+plt.show()
+
+plot_series(time_valid, x_valid)
+
+#%%
+# Naive Forecast
+naive_forecast = series[split_time - 1:-1]
+
+plot_series(time_valid, x_valid)
+plot_series(time_valid, naive_forecast)
+
+plot_series(time_valid, x_valid, start=0, end=125)
+plot_series(time_valid, naive_forecast, start=1, end=151)
+
+print(f'MSE: {mean_squared_error(x_valid, naive_forecast)}')
+print(f'MAE: {mean_absolute_error(x_valid, naive_forecast)}')
+
+### Moving Average
+def moving_average_forecast(series, window_size):
+    """Forecasts the mean of the last few values.
+        If window_size=1, then this is equivalent to naive forecast"""
+    forecast = []
+    for time in range(len(series) - window_size):
+        forecast.append(series[time:time + window_size].mean())
+    return np.array(forecast)
+
+moving_avg = moving_average_forecast(series, 30)[split_time - 30:]
+
+plot_series(time_valid, x_valid)
+plot_series(time_valid, moving_avg)
+
+print(f'MSE: {mean_squared_error(x_valid, moving_avg)}')
+print(f'MAE: {mean_absolute_error(x_valid, moving_avg)}')
+
+### Removing trend and seasonality
+diff_series = (series[365:] - series[:-365])
+diff_time = time[365:]
+
+plot_series(diff_time, diff_series)
+
+##### MA
+diff_moving_avg = moving_average_forecast(diff_series, 25)[split_time - 365 - 25:]
+
+plot_series(time_valid, diff_series[split_time - 365:])
+plot_series(time_valid, diff_moving_avg)
+
+""" Getting actual predictions """
+diff_moving_avg_plus_past = series[split_time - 365:-365] + diff_moving_avg
+
+plot_series(time_train, x_train)
+plot_series(time_valid, x_valid)
+plot_series(time_valid, diff_moving_avg_plus_past)
+
+print(f'MSE: {mean_squared_error(x_valid, diff_moving_avg_plus_past)}')
+print(f'MAE: {mean_absolute_error(x_valid, diff_moving_avg_plus_past)}')
+
+##### Removing past noise
+diff_moving_avg_plus_smooth_past = moving_average_forecast(series[split_time - 370:-360], 10) + diff_moving_avg
+
+plot_series(time_valid, x_valid)
+plot_series(time_valid, diff_moving_avg_plus_smooth_past)
+
+print(f'MSE: {mean_squared_error(x_valid, diff_moving_avg_plus_smooth_past)}')
+print(f'MAE: {mean_absolute_error(x_valid, diff_moving_avg_plus_smooth_past)}')
