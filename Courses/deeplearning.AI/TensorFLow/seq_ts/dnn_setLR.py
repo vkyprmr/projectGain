306b18b39569540d8f7bd3fb247f603aa46c6bec
Vicky Parmar <65457337+vkyprmr@users.noreply.github.com>
2020-09-14 01:34:28 +0200

    updated dnn_setLR.py

--- Courses/deeplearning.AI/TensorFLow/seq_ts/dnn_setLR.py
+++ Courses/deeplearning.AI/TensorFLow/seq_ts/dnn_setLR.py
@@ -123,7 +123,7 @@ plt.ylabel('Loss')
 # Predictions
 forecast = []
 for time in range(len(series) - window_size):
-  forecast.append(model.predict(series[time:time + window_size][np.newaxis]))
+    forecast.append(model.predict(series[time:time + window_size][np.newaxis]))
 
 forecast = forecast[split_time-window_size:]
 results = np.array(forecast)[:, 0, 0]
